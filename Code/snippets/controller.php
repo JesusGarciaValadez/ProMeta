a3ab04f622d2430f86fb23721d1b90863e20f4cc
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2014-12-16 12:24:19 -0600

    No change apparently

--- Code/snippets/controller.php
+++ Code/snippets/controller.php
@@ -3,82 +3,83 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
+if ( $_GET[ 'DEV' ] && $_GET[ 'DEV' ] === 1 ) {
+    error_reporting(E_ALL | E_STRICT);
+    ini_set('display_errors', 1);
+}
+
 if ( file_exists( 'config/config.php' ) ) {
-    
+
     define( 'CURRENT_PATH',dirname(__FILE__) );
     require_once 'config/config.php';
 } else {
-    
+
     exit('no fue posible localizar el archivo de configuración.');
 }
 
 function __autoload( $className ) {
-    
+
     require_once LIBS_PATH . "{$className}.php";
 }
 
 require_once SNIPPETS_PATH . 'db/connection.php';
 
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 1);
-
 if ( ! empty( $_GET['action'] ) ) {
-    
+
     $action = strip_tags( trim( $_GET[ 'action' ] ) );
-    
+
     $data = array();
-    
+
     try {
-        
+
         switch ( $action ) {
-            case 'contact': 
-                
+            case 'contact':
+
                 $toPass[ 'contact_name' ]       = $_POST[ 'contact_name' ];
                 $toPass[ 'contact_business' ]   = $_POST[ 'contact_business' ];
                 $toPass[ 'contact_phone' ]      = $_POST[ 'contact_phone' ];
                 $toPass[ 'contact_mail' ]       = $_POST[ 'contact_mail' ];
                 $toPass[ 'contact_area' ]       = $_POST[ 'contact_area' ];
                 $toPass[ 'contact_message' ]    = $_POST[ 'contact_message' ];
-                
-                $cc = array( 
-                        array( 
-                            'mail'  => 'm.marighi@pro-meta.com.mx', 
-                            'name'  => 'MMarighi'), 
+
+                $cc = array(
                         array(
-                            'mail'  => 'f.corona@pro-meta.com.mx', 
+                            'mail'  => 'm.marighi@pro-meta.com.mx',
+                            'name'  => 'Mauro'),
+                        array(
+                            'mail'  => 'f.corona@pro-meta.com.mx',
                             'name'  => 'FCorona')
                     );
-                
+
                 $doInsert   = new Review( $dbh, '' );
-                $doInsert   = $doInsert->insertInit( 
-                    $toPass, 
-                    "template_mailing.html", 
-                    $_POST[ 'contact_name' ] . " está interesado en pedir información", 
-                    //"m.marighi@pro-meta.com.mx", 
+                $doInsert   = $doInsert->insertInit(
+                    $toPass,
+                    "template_mailing.html",
+                    $_POST[ 'contact_name' ] . " está interesado en pedir información",
                     "info@pro-meta.com.mx",
                     $cc );
                 $data       = json_encode ( $doInsert );
-                
+
                 break;
         }
         echo $data;
-        
+
     } catch ( Exception $e ) {
-        
+
         switch ( $e->getCode() ) {
-            
+
             case 5910 :
                 echo 'DATA BASE ERROR: '.$e->getMessage();
                 $message = 'Lo sentimos, ocurrió un error inesperado al tratar de guardar los datos.';
                 break;
-                
+
             case 5810 :
                 echo 'MAILER ERROR: '. $e->getMessage();
                 $message = 'Lo sentimos, ocurrió un error inesperado al tratar de enviar el correo.';
                 break;
             default : $message = $e->getMessage();
         }
-        
+
         $data = array ('success' => false , 'message' => $message ) ;
         echo json_encode( $data );
     }
--- Code/templates/javascript/main.js
+++ Code/templates/javascript/main.js
@@ -9,108 +9,108 @@
  *
  */
 (function ( $, window, document, undefined ) {
-    
+
     //  When DOM is loaded
     $( function ( ) {
-        
+
         Prometa.init();
-        
+
         if ( $( ".loader" ).exists() ) {
-           
+
             $( '.alert_background' ).fadeOut( 300 );
             $( ".loader" ).fadeOut( 300 );
         }
-        
+
         //  Pone la clase current en el primer texto del home
         if ( $( "#home header" ).exists() ) {
-            
+
             $( "#home header div article" ).first().addClass( 'current' );
         }
-        
+
         //  Control del background
         if ( $( "#wrapper_background" ).exists() ) {
-           
+
             var layers  = $( '#wrapper_background figure' ),
                 controllers = $( 'header a' ),
                 notices = $( 'header div article' ),
                 timeCounter = 0,
                 currentTab, tabLength;
-            
+
             layers.eq(0).addClass( 'current' );
-            
+
             currentTab  = 0;
             tabLength   = layers.length;
-            
+
             //  Controla el cambio de background por medio de las flechas
             $( controllers ).on( 'click', function ( e ) {
-                
+
                 e.preventDefault();
                 e.stopPropagation();
-                
+
                 currentTab  = $( '#wrapper_background figure.current' ).index();
-                
+
                 $( '#wrapper_background figure.current' ).fadeOut( 300, function () {
-                    
+
                     $( this ).toggleClass( 'current' );
                 });
-                
+
                 if ( $( e.currentTarget ).hasClass( 'left' ) ) {
-                    
+
                     if ( currentTab <= 1 ) {
-                        
+
                         layers.eq( 0 ).fadeIn( 300, function () {
-                            
+
                             layers.eq( tabLength - 2 ).addClass( 'current' );
                         });
                         notices.eq( 0 ).fadeIn( 300, function () {
-                            
+
                             notices.removeClass( 'current' ).removeAttr( 'class' );
                             notices.eq( tabLength - 2 ).addClass( 'current' ).removeAttr( 'style' );
                         });
                     } else {
-                        
+
                         layers.eq( currentTab - 2 ).fadeIn( 300, function () {
-                            
+
                             layers.eq( currentTab - 2 ).addClass( 'current' );
                         });
                         notices.eq( currentTab - 2 ).fadeIn( 300, function () {
-                            
+
                             notices.removeClass( 'current' ).removeAttr( 'class' );
                             notices.eq( currentTab - 2 ).addClass( 'current' ).removeAttr( 'style' );
                         });
                     }
                 } else if ( $( e.currentTarget ).hasClass( 'right' ) ) {
-                    
+
                     if ( currentTab >= tabLength ) {
-                        
+
                         layers.eq( 0 ).fadeIn( 300, function () {
-                            
+
                             layers.eq( 0 ).addClass( 'current' );
                         });
                         notices.eq( 0 ).fadeIn( 300, function () {
-                            
+
                             notices.removeClass( 'current' ).removeAttr( 'class' );
                             notices.eq( 0 ).addClass( 'current' ).removeAttr( 'style' );
                         });
                     } else {
-                        
+
                         layers.eq( currentTab ).fadeIn( 300, function () {
-                            
+
                             layers.eq( currentTab ).addClass( 'current' );
                         });
                         notices.eq( currentTab ).fadeIn( 300, function () {
-                            
+
                             notices.removeClass( 'current' ).removeAttr( 'class' );
                             notices.eq( currentTab ).addClass( 'current' ).removeAttr( 'style' );
                         });
                     }
                 }
-                
+
                 $( '#counter' ).css( 'width', 0 + 'px' );
                 timeCounter = 0;
-               
+
                 /*
-               
+
                 //  Obtiene el src de la imagen, busca "_small." en la ruta y lo
                 //  remplaza con "_small_hover." para que la imagen cambie por
                 //  la imagen con hover
@@ -124,111 +124,111 @@
                 $( '#wrapper_background figure.current img' ).attr( 'src', name );
                 */
             });
-            
+
             if ( $( '#timer' ).exists() ) {
-                
+
                 //  Controla el cambio de background aprox. cada 10 segundos.
                 var interval   = setInterval( function ( ) {
-                    
+
                     if ( timeCounter <= 100 ) {
-                        
+
                         timeCounter += 0.1055;  //Sin Math.round
                         //timeCounter += 0.150465;  // Con Math.round
                     } else if ( timeCounter > 100 )  {
-                        
+
                         currentTab  = $( '#wrapper_background figure.current' ).index();
                         $( '#wrapper_background figure.current' ).fadeOut( 300, function () {
-                            
+
                             $( this ).toggleClass( 'current' );
                         });
-                        
+
                         if ( currentTab >= tabLength ) {
-                            
+
                             layers.eq( 0 ).fadeIn( 300, function () {
-                                
+
                                 layers.eq( 0 ).addClass( 'current' );
                             });
                             notices.eq( 0 ).fadeIn( 300, function () {
-                                
+
                                 notices.removeClass( 'current' ).removeAttr( 'class' );
                                 notices.eq( 0 ).addClass( 'current' ).removeAttr( 'style' );
                             });
                         } else {
                             layers.eq( currentTab ).fadeIn( 300, function () {
-                                
-                                
+
+
                                 layers.eq( currentTab ).addClass( 'current' );
                             });
                             notices.eq( currentTab ).fadeIn( 300, function () {
-                                
+
                                 notices.removeClass( 'current' ).removeAttr( 'class' );
                                 notices.eq( currentTab ).addClass( 'current' ).removeAttr( 'style' );
                             });
                         }
                         timeCounter = 0;
                     }
-                    
+
                     $( '#counter' ).css( 'width', timeCounter + '%' );
                 }, 10 );
             }
         }
     } );
-    
+
     //  When DOM is ready
     $( document ).on( 'ready', function ( e ) {
-        
+
         //  Arregla tamaños del background al iniciar y al redimensionar la ventana
         var screenHeight, newHeight;
         var calculateHeight = function ( ) {
-            
+
             screenHeight    = window.innerHeight;
             $( '#wrapper_background' ).height( screenHeight );
             newHeight       = screenHeight - 55;
             $( '#home #nav' ).css( 'top', newHeight + 'px' );
             $( '#header' ).centerWidth();
         };
-        
+
         if ( $( '#wrapper_background' ).exists() ) {
-            
+
             calculateHeight();
-            
+
             $( window ).on( 'resize', function ( e ) {
-                
+
                 calculateHeight();
             } );
         }
-        
+
         if ( $( '#home #nav' ).exists() ) {
-            
+
             $( 'a[title="Home"]' ).on( 'click', function ( e ) {
                 e.preventDefault();
                 e.stopPropagation();
-                
+
                 Prometa.smoothScroll( '#home', 300 );
-                
+
                 $( 'nav ul li' ).removeClass( 'active' );
                 Prometa.toggleClass( $( e.currentTarget ).parent(), 'active' );
-                
+
                 $( '#home' ).css( {
                     overflow: "hidden"
                 } );
             } );
             $( 'a[title="Contacto"]' ).on( 'click', function ( e ) {
-               
+
                 e.preventDefault();
                 e.stopPropagation();
-               
+
                 var topOffset   = $( '#contact_title' ).offset().top - 55;
                 Prometa.smoothScroll( topOffset, 300 );
-               
+
                 $( 'nav ul li' ).removeClass( 'active' );
                 Prometa.toggleClass( $( e.currentTarget ).parent(), 'active' );
-               
+
                 $( '#home' ).css( {
                     overflow: "auto"
                 } );
             } );
-            
+
             /**
              *
              *  Anchor the menu when at loadpage it is in to certain Y coord while doing scroll
@@ -242,13 +242,13 @@
                 position:   'absolute',
                 top:        newHeight + 'px'
             } );
-            
+
             Prometa.anchorMenu( '#home', newHeight, {
                 overflow: "auto"
             }, {
                 overflow: "hidden"
             } );
-            
+
             /**
              *
              *  Anchor the menu when the screen is up to certain Y coord while doing scroll
@@ -256,7 +256,7 @@
              */
             //  !Ancla el menú cuando pasa de cierta coordenada Y al hacer scroll
             $( window ).on( 'scroll', function ( e ) {
-               
+
                 Prometa.anchorMenu( '#home #nav', newHeight, {
                     position:   'fixed',
                     top:        '0px'
@@ -264,16 +264,16 @@
                     position:   'absolute',
                     top:        newHeight + 'px'
                 } );
-               
+
                 if ( Prometa.tool === 0 ) {
-                   
+
                     $( '#home' ).css( {
                         overflow: "hidden"
                     } );
                     $( 'a[title="Home"]' ).parent().addClass( 'active' );
                     $( 'a[title="Contacto"]' ).parent().removeClass( 'active' );
                 } else if ( Prometa.tool >= newHeight ) {
-                   
+
                     $( '#home' ).css( {
                         overflow: "auto"
                     } );
@@ -282,21 +282,21 @@
                 }
             });
         }
-        
+
         //  Construye el Google Maps
         if ( $( '#map' ).exists() ) {
-           
+
             Prometa.makeMapForContact( 19.405234, -99.171704, $( '#map' ) );
-           
+
             $( window ).on( 'resize', function ( e ) {
-               
+
                 Prometa.makeMapForContact( 19.405234, -99.171704, $( '#map' ) );
             } );
         }
-        
+
         //  !Crea una instancia de jQuery Overlay
         if ( $( '.alert_box' ).exists() ) {
-            
+
             Prometa.doOverlay( $( 'a.alert_trigger' ), {
                 effect: 'apple',
                 close: $( '.alert_box a.close' ),
@@ -305,7 +305,7 @@
                 speed: 'normal',
                 fixed: true,
                 onBeforeLoad: function ( e ) {
-                    
+
                     $( '.alert_background' ).height( '100%' );
                     ( $( '.alert_box a.close' ).exists() ) ? true : $( '.alert_box' ).prepend( Prometa.closer );
                     $( '.alert_box' ).centerWidth();
@@ -316,9 +316,9 @@
                     $( '.alert_background' ).fadeIn( 100 );
                 },
                 onBeforeClose:  function ( ){
-                    
+
                     $( '.alert_box' ).fadeOut( 10, function ( ) {
-                        
+
                         $( '.alert_background' ).fadeOut( 10 );
                         /*$( '.alert_box h2' ).text( '' );
                         $( '.alert_box h4' ).text( '' );
@@ -332,42 +332,42 @@
                 },
                 onClose: function ( e ) {}
             } );
-            
+
             Prometa.overlay    = $( '.alert_trigger' ).data( 'overlay' );
-            
+
             //$( '.alert_background' ).height( $( 'body' ).height() );
         }
-        
+
         //  Crea una instancia de jQuery Overlay para el home de descubreone.mx
         //  Calcula la distancia entre el margen izquierdo para posicionar
         //  la capa del video. Si en menor de 0 (ocurre en iPhone) utiliza
         //  el ancho del body en vez del ancho de la ventana para hacer
         //  el cálculo
         if ( $( '.overlay' ).exists() ) {
-            
+
             $( '.overlay' ).centerWidth();
-            
+
             $( window ).on( {
                 resize: function ( e ) {
-                   
+
                     $( '.overlay' ).centerWidth();
                 },
                 touchstart: function ( e ) {
-                   
+
                     $( '.overlay' ).centerWidth();
                 },
                 touchend: function ( e ) {
-                   
+
                     $( '.overlay' ).centerWidth();
                 }
             } );
         }
-        
+
         // Validación de los formularios
         if ( $( 'form' ).exists() ) {
-            
+            console.log('Uniform');
             Prometa.makesUniform( 'select' );
-            
+
             var rules   = {
                     contact_name:       {
                         required:   true,
@@ -408,29 +408,29 @@
                     number: "Escriba solo números",
                     digits: "Escriba solo números",
                 };
-           
+
             Prometa.validateForms( rules, messages );
         }
-        
+
         //  Handler de contenido de textarea
         if ( $( 'textarea' ).exists() ) {
-            
+
             $( 'textarea' ).val( 'Mensaje' );
             Prometa.toggleValue( 'textarea', "Mensaje" );
         }
-        
+
         //  Hace el efecto de placeholder manejando los values de los inputs
         if ( $( 'input[type="text"]' ).exists() ) {
-            
+
             $( 'input[type="text"]' ).map( function ( index, DOMElement ) {
                 var _value  = $( DOMElement ).val();
                 Prometa.toggleValue( $( DOMElement ), _value );
             } );
         }
-        
+
         //  Carruseles y efectos del Home
         if ( $( '#one' ).exists() ) {
-            
+
             Prometa.inicializeCarrousel( '#header_scrollable', {
                 speed: 1000,
                 circular: true,
@@ -466,10 +466,10 @@
                 autopause: true
             } );
         }
-        
+
         //  Carruseles y efectos de sección Servicios
         if ( $( '#services' ).exists() ) {
-            
+
             Prometa.inicializeCarrousel( '.services_subtitle .information.scrollable', {
                 speed: 500,
                 circular: false,
@@ -504,50 +504,50 @@
                 autoplay: false,
                 autopause: false
             } );
-            
+
             var carrouselSubtitle           = $( '.services_subtitle' );
             var carrouselSubtitleScrollable = carrouselSubtitle.find( '.information.scrollable' ).data( 'scrollable' );
             var carrouselController         = $( '#services_descriptions .information.scrollable' ).data( 'scrollable' );
             var trigger;
-            
+
             $( '#services_descriptions .scrollable a' ).on( 'click', function ( e ) {
-                
+
                 if ( $( e.currentTarget).hasClass( 'next' ) ) {
                     trigger = "next";
                 } else if ( $( e.currentTarget).hasClass( 'next' ) ) {
                     trigger = "prev";
                 }
             } );
-            
+
             //  Hace la animación de retraso para el título
             carrouselController.onBeforeSeek( function ( e ) {
-                
+
                 var carrouselIndex      = carrouselController.getIndex();
                 var changeBackground    = function () {
                     if ( carrouselSubtitle.hasClass( 'consultancy' ) ) {
-                        
+
                         carrouselSubtitle.removeClass( 'consultancy' ).addClass( 'eficiency' );
                         carrouselSubtitleScrollable.next( 500 );
                     } else if ( carrouselSubtitle.hasClass( 'eficiency' ) )  {
-                        
+
                         carrouselSubtitle.removeClass( 'eficiency' ).addClass( 'consultancy' );
                         carrouselSubtitleScrollable.prev( 500 );
                     }
                     carrouselSubtitle.dequeue().fadeIn( 250 );
-                }; 
-                
+                };
+
                 setTimeout( function () {
-                    
+
                     if ( trigger === "next" ) {
-                        
+
                         carrouselSubtitle.dequeue().fadeOut( 250, changeBackground );
                     } else if ( trigger === "prev" ) {
-                        
+
                         carrouselSubtitle.dequeue().fadeOut( 250, changeBackground );
                     }
                 }, 200 );
             } );
         }
     } );
-    
+
 })( jQuery, window, document );
